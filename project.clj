234467329f05d862b1592ba605ca8f337a87f5bb
lenz <lenz@lenzs-MacBook-Pro.local>
2015-04-26 15:21:15 +0200

    Added Marginalia for docs

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject so_long_ow2 "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "?"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot so-long-ow2.core
   :target-path "target/%s"
+  :plugins [[lein-marginalia "0.8.0"]]
   :profiles {:uberjar {:aot :all}})
