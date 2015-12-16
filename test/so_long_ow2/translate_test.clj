ac15687c5f4f238be4363565e1cdce0db4e86608
lenz <lenz@lenzs-MacBook-Pro.local>
2015-12-16 15:03:50 +0100

    Making tests pass

--- test/so_long_ow2/translate_test.clj
+++ test/so_long_ow2/translate_test.clj
@@ -6,11 +6,19 @@
 ;  (testing "FIXME, I fail."
 ;    (is (= 3 (hello)))))
 
+(def NODEHASH
+  '( {:title "QueueMetrics" :fname "DT-QM"}
+     {:title "Pippo" :fname "DT-PIPPO"}
+     {:title "Pluto" :fname "DT-PLUTO"}
+     {:title "Lnk"   :fname "node_lnk.md"} ))
+
+
+
 (deftest codeblk
   (testing "Block CODE"
     (let [src "hello\n[#CODE:]hithere\nyothere[:CODE#]\nx"
           dst "hello\n    hithere\n    yothere\n\nx"
-          prc (t/translate-ow2 src)
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
 
@@ -18,15 +26,15 @@
   (testing "No plugins"
     (let [src "hello\n[#hithere\nyothere#]\nx"
           dst "hello\nNO-PLUGIN:(hithere\nyothere)\nx"
-          prc (t/translate-ow2 src)
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
 
 (deftest double-links
   (testing "Double links"
     (let [src "hello [lnk|txt]\nx"
-          dst "hello [txt](node_lnk.md)\nx"
-          prc (t/translate-ow2 src)
+          dst "hello [txt]({% post_url node_lnk.md %})\nx"
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
 
@@ -34,15 +42,15 @@
   (testing "Monos"
     (let [src "hello [x] [y]\nx"
           dst "hello <x> <y>\nx"
-          prc (t/translate-ow2 src)
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
 
 (deftest single-links
   (testing "Single links"
     (let [src "hello [Lnk]\nx"
-          dst "hello [Lnk](node_lnk.md)\nx"
-          prc (t/translate-ow2 src)
+          dst "hello [Lnk]({% post_url node_lnk.md %})\nx"
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
 
@@ -51,7 +59,7 @@
 (deftest two-links-are-broken
   (testing "2 links are broken"
     (let [src "[*] [http://www.grandstream.com/|Grandstream web site]"
-          dst "<*> [Grandstream web site](http://www.grandstream.com/)"
-          prc (t/translate-ow2 src)
+          dst "\n* [Grandstream web site](http://www.grandstream.com/)"
+          prc (t/translate-ow2 src NODEHASH)
           ]
       (is (= dst prc)))))
